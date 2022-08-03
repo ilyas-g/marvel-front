4d2aeffffdd0d4d9a4f4e418a75ed9c8ed092a6a
Roddoark <ilyas150691@hotmail.fr>
2022-08-03 12:40:06 +0200

    Récupération et afffichage des personnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, React } from 'react';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    //Avec un tableau vide en deuxième argument
+    // La fonction useEffect ne sera déclenchée qu'une fois fois, au chargement du composant
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/characters");
+        setData(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading === true ? (
+        <h1>En cours de chargement</h1>
+      ) : (
+        <>
+          {data.results.map((character, index) => {
+            const imgSrc = character.thumbnail.path + '.' + character.thumbnail.extension;
+            return (
+              <div key={index}>
+                <p>{character.name}</p>
+                <p>{character.description}</p>
+                <img src={imgSrc} alt={character.name} />
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
