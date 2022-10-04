8ea90f544a230e1486d4bc7d640c96918d3caa36
ilyas-g <ilyas15069i@gmail.com>
2022-10-04 15:35:15 +0200

    Typescript working

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,14 +3,9 @@ import './style.scss';
 import logo from '../../assets/images/logo.png';
 import { Link } from "react-router-dom";
 
-interface Props {
-    name: string;
-  }
-
-export default function Header(props: Props): JSX.Element | null {
+export default function Header(): JSX.Element | null {
     return (
         <header>
-            <div>{props.name}</div>
             <div className='headerLeft'>
                 <Link to='/'><img className='logo' src={logo} alt="Marvel" /></Link>
                 <nav>
